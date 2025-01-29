b6392473ac85ba60656cb5ec52f7b6d8ffde2c2c
CrucksKudos <29085362+CrucksKudos@users.noreply.github.com>
2025-01-29 14:49:16 -0300

    Finish reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let finalString = "";
+    for (i = word.length - 1; i >= 0; i--)  {
+        finalString += word.charAt(i);
+    }
+    return finalString;
 };
 
 // Do not edit below this line
