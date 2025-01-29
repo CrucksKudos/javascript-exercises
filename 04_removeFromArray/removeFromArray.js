ca570ba56bdc01a58bda9f0bdfe8063c618d2a23
CrucksKudos <29085362+CrucksKudos@users.noreply.github.com>
2025-01-29 16:19:56 -0300

    Finish removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(toRemoveArray, ...toBeRemoved) {
+    for (i = 0; i < toBeRemoved.length; i++) {
+        for (j = 0; j < toRemoveArray.length; j++) {
+            if (toRemoveArray[j] === toBeRemoved[i]) {
+                toRemoveArray.splice (j,1);
+                j--;
+            }
+        }
+    }
+    return toRemoveArray;
 };
 
-// Do not edit below this line
+console.log(removeFromArray([1, 2, 2, 3], 2))
+
+// // Do not edit below this line
 module.exports = removeFromArray;
