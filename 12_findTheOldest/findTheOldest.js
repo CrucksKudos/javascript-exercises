6571eae1f7ceae61a6034e2cb33b655ac309a11b
CrucksKudos <29085362+CrucksKudos@users.noreply.github.com>
2025-01-31 18:38:17 -0300

    Finish findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
+const getAge = (person) => {
+    return (person.yearOfDeath ?? new Date().getFullYear()) - person.yearOfBirth;
+};
 
+const findTheOldest = function (people) {
+    return people.sort((a, b) => getAge(b) - getAge(a))[0];
 };
 
 // Do not edit below this line
