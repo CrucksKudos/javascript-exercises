5180db94cfb022357184dd30a60bdac44cd5d193
CrucksKudos <29085362+CrucksKudos@users.noreply.github.com>
2025-01-31 18:05:33 -0300

    Finish Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    
+    fibo = [0,1];
+    for (let i = 2; i <= n; i++) {
+        fibo.push (fibo[i-2] + fibo [i-1])
+    }
+    return fibo[n]
 };
 
 // Do not edit below this line
