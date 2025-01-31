d43a9aec9d76bc5237f5110b9cdc5e3014b13ed5
CrucksKudos <29085362+CrucksKudos@users.noreply.github.com>
2025-01-31 17:48:30 -0300

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const onlyAlpha = [... string].filter(char => /[^\W_]/.test(char)).join('').toLowerCase();
+    console.log(onlyAlpha);
+    for (let i = 0; i < onlyAlpha.length / 2; i++) {
+        if (onlyAlpha[i] !== onlyAlpha[onlyAlpha.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
